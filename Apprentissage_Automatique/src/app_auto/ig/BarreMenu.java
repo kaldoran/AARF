695cdb7bc3c8365cad600f9461cad45dad6e2de8
Kaldoran <kaldoran@live.fr>
2015-02-11 02:03:12 +0100

    Ajout JMenu pour la configuration du KPP

--- Apprentissage_Automatique/src/app_auto/ig/BarreMenu.java
+++ Apprentissage_Automatique/src/app_auto/ig/BarreMenu.java
@@ -9,21 +9,34 @@ import app_auto.utils.IgConstante;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.KeyStroke;
 
 /**
  *
  * @author kaldoran
  */
-
 public class BarreMenu extends JMenuBar implements ActionListener {
 
     private JMenu menu_fichier;
     private JMenuItem mfich_nouveau;
     private JMenuItem quitter;
+
+    private JMenu kpp;
+    private ButtonGroup distance;
+    private JCheckBoxMenuItem manhattan;
+    private JCheckBoxMenuItem euclidienne;
+    private JMenu kpp_value;
+
+    private ButtonGroup number;
+    private JCheckBoxMenuItem three;
+    private JCheckBoxMenuItem five;
+    private JCheckBoxMenuItem seven;
 
     public BarreMenu() {
         super();
@@ -55,6 +68,36 @@ public class BarreMenu extends JMenuBar implements ActionListener {
         menu_fichier.add(quitter);
 
         this.add(menu_fichier);
+        kpp = new JMenu("KPP Configuration");
+        euclidienne = new JCheckBoxMenuItem("Euclidienne");
+        manhattan = new JCheckBoxMenuItem("manhattan");
+
+        kpp.add(euclidienne);
+        kpp.add(manhattan);
+
+        distance = new ButtonGroup();
+        distance.add(euclidienne);
+        distance.add(manhattan);
+
+        kpp_value = new JMenu("Kpp Value");
+        three = new JCheckBoxMenuItem("3");
+        five = new JCheckBoxMenuItem("5");
+        seven = new JCheckBoxMenuItem("7");
+
+        kpp_value.add(three);
+        kpp_value.add(five);
+        kpp_value.add(seven);
+
+        number = new ButtonGroup();
+        number.add(three);
+        number.add(five);
+        number.add(seven);
+
+        manhattan.setSelected(true);
+        three.setSelected(true);
+
+        kpp.add(kpp_value);
+        this.add(kpp);
     }
 
     @Override
